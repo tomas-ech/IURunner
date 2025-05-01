c8ef4c4e44381de854937e60c2f45e4c83f6da05
tomas96ech <techeverri96@gmail.com>
2025-05-01 18:34:01 -0500

    feat: player basic ui

---
+++ Assets/Scripts/UI/UiManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private Image lifeBar;
+    [SerializeField] private Image expBar;
+    [SerializeField] private TextMeshProUGUI score;
+}
