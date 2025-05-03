b94438d9490dd5301f061e7662a17f8eda9369b4
tomas96ech <techeverri96@gmail.com>
2025-05-03 17:46:21 -0500

    refactor: changed enemy logic
    added: enemy state machine

--- Assets/Scripts/Obstacles/Enemies/Enemy.cs
+++ Assets/Scripts/Obstacles/Enemies/Enemy.cs
@@ -6,9 +6,44 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int damage = 25;
 
     [SerializeField] protected bool canDoDamage = true;
-    [SerializeField] protected Animator animator;
+
+    public Animator animator;
+    public Rigidbody2D rigidBody;
+
+    [Header("Collisions")]
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private Transform wallChecker;
+    [SerializeField] private float wallDistance;
+
+    [SerializeField] private LayerMask groundMask;
+
+    public EnemyStateMachine stateMachine {  get; private set; }
 
     protected bool isForward = true;
+    private float currentDirection = 1f;
+
+    private void Awake()
+    {
+        stateMachine = new EnemyStateMachine();
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+
+    public bool IsOnGround()
+    {
+        bool isGround = Physics2D.Raycast(groundChecker.position, Vector2.down, groundDistance, groundMask);
+        return isGround;
+    }
+    
+    public bool IsAtWall()
+    {
+        bool isAWall = Physics2D.Raycast(wallChecker.position, Vector2.right * currentDirection, wallDistance, groundMask);
+        return isAWall;
+    }
 
     protected void DoDamage(Collider2D collision)
     {
@@ -20,6 +55,7 @@ public class Enemy : MonoBehaviour
 
     protected void ChangeDirection()
     {
+        currentDirection *= -1;
         isForward = !isForward;
         animator.gameObject.transform.Rotate(0, 180, 0);
 
@@ -33,5 +69,11 @@ public class Enemy : MonoBehaviour
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         DoDamage(collision);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundChecker.position, new Vector3(groundChecker.position.x, groundChecker.position.y - groundDistance));
+        Gizmos.DrawLine(wallChecker.position, new Vector3(wallChecker.position.x + wallDistance, wallChecker.position.y));
     }
 }
